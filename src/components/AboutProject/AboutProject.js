3da7d305190a82ba93be2e97446ac9356805ae06
George Gorchev <georgegorchev@yandex.ru>
2021-02-25 23:57:32 +0300

    fix: AboutProject

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -4,18 +4,22 @@ import './AboutProject.css';
 
 function AboutProject() {
   return (
-    <Section title="О проекте" light="true" anchor="aboutProject">
+    <Section
+      title="О проекте"
+      light="true"
+      anchor="aboutProject"
+    >
       <div className="about-project__grid">
-        <div className="about-project__grid-item">
+        <div>
           <h3 className="about-project__header">Дипломный проект включал 5 этапов</h3>
-          <p className="about-project__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+          <p className="about-project__description">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
         </div>
-        <div className="about-project__grid-item">
+        <div>
           <h3 className="about-project__header">На выполнение диплома ушло 5 недель</h3>
-          <p className="about-project__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+          <p className="about-project__description">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
         </div>
       </div>
-      <div className="about-project__progress___grid">
+      <div className="about-project__progress-bar">
         <span className="about-project__backend" />
         <span className="about-project__frontend" />
       </div>
