965511191048e5b9e46cfa34fff74495d94e04e8
George Gorchev <georgegorchev@yandex.ru>
2021-02-26 02:08:44 +0300

    fix: Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,6 @@
-/* eslint-disable */
 import React from 'react';
 import Header from '../Header/Header';
-import '../Profile/Profile.css';
+import './Profile.css';
 
 function Profile() {
   const [name, setName] = React.useState('Виталий');
@@ -9,31 +8,38 @@ function Profile() {
 
   return (
     <>
-    <Header />
-    <div className="profile">
-      <h1 className="profile__header">Привет, Виталий!</h1>
-      <form className="profile__form">
-        <label className="profile__label" for="name">Имя</label>
-        <input
-        className="profile__input"
-        type="text" id="name"
-        value={name}
-        onChange={(e) => {setName(e.target.value)}}
-        />
-        <hr className="profile__divider" />
-        <label className="profile__label" for="email">Почта</label>
-        <input
-          className="profile__input"
-          type="text" id="email"
-          value={email}
-          onChange={(e) => {setEmail(e.target.value)}}
-        />
-      </form>
-      <div className="profile__buttons">
-      <button className="profile__edit-button" type="submit">Редактировать</button>
-      <button className="profile__signout-button" type="button">Выйти из аккаунта</button>
+      <Header />
+      <div className="profile">
+        <h1 className="profile__header">Привет, Виталий!</h1>
+        <form className="profile__form" id="profile">
+          <label className="profile__label" htmlFor="name">
+            Имя
+            <input
+              className="profile__input"
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => { setName(e.target.value); }}
+            />
+          </label>
+          <hr className="profile__divider" />
+          <label className="profile__label" htmlFor="email">
+            Почта
+
+            <input
+              className="profile__input"
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => { setEmail(e.target.value); }}
+            />
+          </label>
+        </form>
+        <div className="profile__buttons">
+          <button className="profile__button profile__button_type_submit" type="submit" form="profile">Редактировать</button>
+          <button className="profile__button profile__button_type_logout" type="button">Выйти из аккаунта</button>
+        </div>
       </div>
-    </div>
     </>
   );
 }
