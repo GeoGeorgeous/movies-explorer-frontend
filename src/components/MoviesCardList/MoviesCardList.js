200ff00933d54282f01d3a0dbd78300c99c50655
George Gorchev <georgegorchev@yandex.ru>
2021-02-26 02:58:33 +0300

    fix: MoviesCardList

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,28 +1,26 @@
-/* eslint-disable */
 import React from 'react';
 import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
 import moviesDB from '../../utils/moviesDB';
 import Footer from '../Footer/Footer';
 
-function MoviesCardList(props) {
-  const {includeShortMovies} = props;
+function MoviesCardList() {
   return (
     <>
-    <section className="movies-card-list">
-      {moviesDB && moviesDB.map( (movie) => (
-        <MoviesCard
-          key={movie._id}
-          uniqueId={movie._id}
-          duration={movie.duration}
-          cover={movie.cover}
-          title={movie.title}
-          isFavourite={movie.isFavourite}
-        />
-      ))}
-    </section>
-    <button className="movies-card-list__load-more">Ещё</button>
-    <Footer />
+      <section className="movies-card-list">
+        {moviesDB && moviesDB.map((movie) => (
+          <MoviesCard
+            key={movie._id}
+            uniqueId={movie._id}
+            duration={movie.duration}
+            cover={movie.cover}
+            title={movie.title}
+            isFavourite={movie.isFavourite}
+          />
+        ))}
+      </section>
+      <button className="movies-card-list__load-more" type="button">Ещё</button>
+      <Footer />
     </>
   );
 }
