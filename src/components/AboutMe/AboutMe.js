2e6e5c49dd7e3c007fd16c586ae9d7b9c79ebf3c
George Gorchev <georgegorchev@yandex.ru>
2021-02-26 23:45:40 +0300

    feat: Portfolio Component and errors fix

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Section from '../Section/Section';
+import Portfolio from '../Portfolio/Portfolio';
 import './AboutMe.css';
 import avatar from '../../images/avatar.png';
 
@@ -9,46 +10,31 @@ function AboutMe() {
       title="Студент"
       anchor="aboutMe"
     >
-      <div className="about-me__container">
-        <div className="about-me__author">
-          <h3 className="about-me__header">Виталий</h3>
-          <p className="about-me__subtitle">Фронтенд-разработчик, 30 лет</p>
-          <p className="about-me__description">
-            Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
-            и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом.
-            Недавно начал кодить. С 2015 года работал в компании «СКБ Контур».
-            После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами
-            и ушёл с постоянной работы.
-          </p>
-          <ul className="about-me__social-links">
-            <li className="about-me__link-item">
-              <a className="about-me__social-link" href="https://www.facebook.com/" target="_blank" rel="noreferrer">Facebook</a>
-            </li>
-            <li className="about-me__link-item">
-              <a className="about-me__social-link" href="https://github.com/GeoGeorgeous" target="_blank" rel="noreferrer">Github</a>
-            </li>
-          </ul>
+      <>
+        <div className="about-me__container">
+          <div className="about-me__author">
+            <h3 className="about-me__header">Виталий</h3>
+            <p className="about-me__subtitle">Фронтенд-разработчик, 30 лет</p>
+            <p className="about-me__description">
+              Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
+              и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом.
+              Недавно начал кодить. С 2015 года работал в компании «СКБ Контур».
+              После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами
+              и ушёл с постоянной работы.
+            </p>
+            <ul className="about-me__social-links">
+              <li className="about-me__link-item">
+                <a className="about-me__social-link" href="https://www.facebook.com/" target="_blank" rel="noreferrer">Facebook</a>
+              </li>
+              <li className="about-me__link-item">
+                <a className="about-me__social-link" href="https://github.com/GeoGeorgeous" target="_blank" rel="noreferrer">Github</a>
+              </li>
+            </ul>
+          </div>
+          <img className="about-me__avatar" src={avatar} alt="Фотография автора" />
         </div>
-        <img className="about-me__avatar" src={avatar} alt="Фотография автора" />
-      </div>
-      <h4 className="about-me__portfolio-header">Портфолио</h4>
-      <ul className="about-me__project-buttons">
-        <li className="about-me__project-button-item">
-          <a href="https://github.com/GeoGeorgeous/how-to-learn" target="_blank" rel="noreferrer">
-            <button className="about-me__project-button" type="button">Статичный сайт</button>
-          </a>
-        </li>
-        <li className="about-me__project-button-item">
-          <a href="https://github.com/GeoGeorgeous/russian-travel" target="_blank" rel="noreferrer">
-            <button className="about-me__project-button" type="button">Адаптивный сайт</button>
-          </a>
-        </li>
-        <li className="about-me__project-button-item">
-          <a href="https://mesto.students.nomoredomains.work/" target="_blank" rel="noreferrer">
-            <button className="about-me__project-button" type="button">Одностраничное приложение</button>
-          </a>
-        </li>
-      </ul>
+        <Portfolio />
+      </>
     </Section>
   );
 }
--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -9,20 +9,22 @@ function AboutProject() {
       light="true"
       anchor="aboutProject"
     >
-      <div className="about-project__grid">
-        <div>
-          <h3 className="about-project__header">Дипломный проект включал 5 этапов</h3>
-          <p className="about-project__description">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+      <>
+        <div className="about-project__grid">
+          <div>
+            <h3 className="about-project__header">Дипломный проект включал 5 этапов</h3>
+            <p className="about-project__description">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+          </div>
+          <div>
+            <h3 className="about-project__header">На выполнение диплома ушло 5 недель</h3>
+            <p className="about-project__description">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+          </div>
         </div>
-        <div>
-          <h3 className="about-project__header">На выполнение диплома ушло 5 недель</h3>
-          <p className="about-project__description">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        <div className="about-project__progress-bar">
+          <span className="about-project__backend" />
+          <span className="about-project__frontend" />
         </div>
-      </div>
-      <div className="about-project__progress-bar">
-        <span className="about-project__backend" />
-        <span className="about-project__frontend" />
-      </div>
+      </>
     </Section>
   );
 }
