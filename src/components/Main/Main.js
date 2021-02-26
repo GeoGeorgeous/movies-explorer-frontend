b984c7d5f2e4fc90e1691b88c5ca941113a91fa2
George Gorchev <georgegorchev@yandex.ru>
2021-02-27 02:10:00 +0300

    feat: adaptive: AboutProject, Section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Hero from '../Hero/Hero';
-// import AboutProject from '../AboutProject/AboutProject';
+import AboutProject from '../AboutProject/AboutProject';
 // import Techs from '../Techs/Techs';
 // import AboutMe from '../AboutMe/AboutMe';
 // import Footer from '../Footer/Footer';
@@ -13,8 +13,8 @@ function Main() {
         loggedIn={false}
       />
       <Hero />
-      {/* <AboutProject />
-      <Techs />
+      <AboutProject />
+      {/* <Techs />
       <AboutMe />
       <Footer /> */}
     </>
