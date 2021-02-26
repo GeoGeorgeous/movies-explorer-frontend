5118d2149b96539283e05bd900977e6a4d555249
George Gorchev <georgegorchev@yandex.ru>
2021-02-26 03:04:12 +0300

    fix: MoviesCard

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import './MoviesCard.css';
 import PropTypes from 'prop-types';
@@ -9,34 +9,41 @@ function MoviesCard(props) {
   } = props;
 
   const [saved, setSaved] = React.useState(isFavourite);
-  
+
   MoviesCard.propTypes = {
-    duration: PropTypes.number.isRequired,
-    cover: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    isFavourite: PropTypes.bool.isRequired,
+    uniqueId: PropTypes.string.isRequired, // Уникальный ID для добавления/удаления из сохранённых
+    duration: PropTypes.number.isRequired, // Длительность в минутах
+    cover: PropTypes.string.isRequired, // Изображение карточки
+    title: PropTypes.string.isRequired, // Название фильма
+    isFavourite: PropTypes.bool.isRequired, // Добавлено в любимые? Bool
   };
 
+  /*
+    Вычисляет длительность фильма в часах и минутах
+    и возвращает строку вида '2ч 14м'
+
+    @param movieDurationInMinutes: длительность в минутах
+  */
   function calcDuration(movieDurationInMinutes) {
     const hours = Math.floor(movieDurationInMinutes / 60);
     const minutes = Math.floor(movieDurationInMinutes - hours * 60);
-    return `${hours}ч ${minutes}м`
+    return `${hours}ч ${minutes}м`;
   }
 
-  function handleMovieClick(e) {
+  function handleMovieClick() {
     setSaved(!saved);
   }
 
-  const inputId = `favourite${uniqueId}`
+  const inputId = `favourite${uniqueId}`;
 
   return (
     <article className="movies-card">
-      <label for={inputId} className="movies-card__cover">
-        <img src={cover} className="movies-card__cover-image" />
+      <label htmlFor={inputId} className="movies-card__cover">
+        <img src={cover} className="movies-card__cover-image" alt={title} />
       </label>
       <h4 className="movies-card__title">{title}</h4>
       <span className="movies-card__duration">{calcDuration(duration)}</span>
-      <input className="movied-card__save-button" type="checkbox" id={inputId} onClick={handleMovieClick} checked={saved}/>
+      <input className="movied-card__save-button" type="checkbox" id={inputId} onClick={handleMovieClick} checked={saved} />
     </article>
   );
 }
